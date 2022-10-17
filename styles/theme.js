f2bc098f80285063f62bf14a770529d904139469
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-10-17 21:49:35 +0900

    theme fix

--- styles/theme.js
+++ styles/theme.js
@@ -1,69 +1,37 @@
-// const headerColor = {
-// 	thema1: '#c85a54',
-// 	thema2: '#9c64a6',
-// 	thema3: '#49a7cc',
-// 	thema4: '#88c399',
-// };
-// const mainColor = {
-// 	thema1: '#b64fc8',
-// 	thema2: '#ea80fc',
-// 	thema3: '#80d8ff',
-// 	thema4: '#b9f6ca',
-// };
-
-// const itemColor = {
-// 	thema1: '#ffbcaf',
-// 	thema2: '#ffb2ff',
-// 	thema3: '#b5ffff',
-// 	thema4: '#ecfffd',
-// };
 export const thema1 = {
-	header: '#c85a54',
-	main: '#b64fc8',
-	item: '#ffbcaf',
+	header: '#373d7b', //헤더 프로필 블록
+	main: '#9695dd', //bakcgrond_color
+	item: '#6667ab', //미정
 };
 export const thema2 = {
-	header: '#9c64a6',
-	main: '#ea80fc',
-	item: '#ffb2ff',
+	header: '#656769',
+	main: '#c3c6c8',
+	item: '#939597',
 }; //
 export const thema3 = {
-	header: '#49a7cc',
-	main: '#80d8ff',
-	item: '#b5ffff',
+	header: '#ac9c8e',
+	main: '#fffff1',
+	item: '#decdbe',
 };
 export const thema4 = {
-	header: '#88c399',
-	main: '#b9f6ca',
-	item: '#ecfffd',
+	header: '#a31545',
+	main: '#ff819e',
+	item: '#d94f70',
 };
+
 const deviceSizes = {
-	mobile: '400px', // 1fr 1
-	tablet: '768px', // 1fr 2
-	laptop: '1024px', // 1fr 3
+	s: '500px',
+	m: '768px',
+	l: '1024px',
 };
 
 const device = {
-	mobile: `screen and (max-width: ${deviceSizes.mobile})`,
-	tablet: `screen and (min-width: ${deviceSizes.mobile}) and (max-width: ${deviceSizes.tablet})`,
-	laptop: `screen and (min-width : ${deviceSizes.tablet})`,
+	mobile: `screen and (max-width: ${deviceSizes.s})`,
+	tablet: `screen and (min-width: ${deviceSizes.s}) and (max-width: ${deviceSizes.l})`,
+	laptop: `screen and (min-width : ${deviceSizes.l})`,
 };
 
-const colors = {
-	black: '#1e1f1d',
-	yellow: '#edb83c',
-	orange: '#eb7952',
-	gray: '#6e6e6e',
-	gray_background: '#f5f5f5',
-	bakcgrond_color: '#c1a0ff',
-	theme_color1: '#99FFFF',
-	theme_color2: '#FF9966',
-	theme_color3: '#6600FF',
-	theme_color4: '#990033',
-};
 const theme = {
-	// 후에 태마 색
-	colors,
 	device,
 	thema1,
 	thema2,
