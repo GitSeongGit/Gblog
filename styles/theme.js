2bb237c6f643a0aefa0b5c076d1a1c30cee9aa3d
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-10-30 19:23:56 +0900

    d

--- styles/theme.js
+++ styles/theme.js
@@ -1,18 +1,22 @@
+//very peri
 export const thema1 = {
 	header: '#373d7b',
 	main: '#9695dd',
 	item: '#6667ab',
 };
+//Ultimate Gray
 export const thema2 = {
 	header: '#656769',
 	main: '#c3c6c8',
 	item: '#939597',
 };
+//Sand Dollar
 export const thema3 = {
 	header: '#ac9c8e',
 	main: '#fffff1',
 	item: '#decdbe',
 };
+//HoneySuckle
 export const thema4 = {
 	header: '#a31545',
 	main: '#ff819e',
