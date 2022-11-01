45bf13b3ab7f4c10c4a02fcf8c0a68c3b30c803b
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-11-02 05:38:59 +0900

    README SET

--- components/themeChange.js
+++ components/themeChange.js
@@ -65,25 +65,25 @@ const BackGround = styled.div`
 	width: 100%;
 `;
 
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	width: 30rem;
-	height: 30rem;
-	background: whitesmoke;
-	border-top: none;
-	border-left: none;
-	border-right: none;
-	border-bottom: 0.1rem, #494949b6, solid;
-	border-radius: 3rem;
-	justify-content: center;
-	align-items: center;
-	z-index: 999;
-`;
+// const Container = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	position: fixed;
+// 	top: 50%;
+// 	left: 50%;
+// 	transform: translate(-50%, -50%);
+// 	width: 30rem;
+// 	height: 30rem;
+// 	background: whitesmoke;
+// 	border-top: none;
+// 	border-left: none;
+// 	border-right: none;
+// 	border-bottom: 0.1rem, #494949b6, solid;
+// 	border-radius: 3rem;
+// 	justify-content: center;
+// 	align-items: center;
+// 	z-index: 999;
+// `;
 
 const Button = styled.button`
 	border: 0.5px solid;
